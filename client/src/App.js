a3d748555e7a8f19c683d494e5169a56e92a9d34
unknown <iamfarmanmalik@gmail.com>
2022-12-20 21:24:28 +0530

    File updated

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   const token=localStorage.getItem("JWT_TOKEN");
   const dispatch=useDispatch()
   useEffect(()=>{
-      if(token){
+      if(token?.length>0){
         dispatch(Authenticated())
       }
   },[])
